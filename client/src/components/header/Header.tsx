dc9eabe9dc2f1f75f12125baf7ff9a617f5e3b0e
unknown <thumpel81@gmail.com>
2024-07-31 22:59:19 +0100

    done animation click

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,10 +1,21 @@
 import './Header.scss'
+import { NavLink } from 'react-router-dom'
 export default function Header() {
   return (
     <header>
         <div className='block_header'>
             <img src="../../public/img/coin.png" className="sigma_img" alt="error"/>
             <h1>SIGMA COIN</h1>
+        </div>
+        <div className='list-bar'>
+          <ul>
+            <li>sigmas</li>
+            <li>add sigma</li>
+            <li>about us</li>
+          </ul>
+        </div>
+        <div className='profile_icon'>
+          <img src='../../public/img/profile.png' alt='user'/>
         </div>
     </header>
   )
--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -1,11 +1,24 @@
-import './Main.scss'
-import { useAuth } from '../../contexts/AuthContext'
+import "./Main.scss";
+import { useState } from "react";
+import sigma from "../../../public/img/sigma.webp";
+import onclick from "../../../public/img/onclick.jpg"
 export default function Main() {
-  const {logout} = useAuth();
+  const [count, setCount] = useState<number>(0);
+  const [image, setImage] = useState(sigma);
+
+  const clickCoin = () => {
+      setCount(count+1);
+      setImage(prevImage=>prevImage===sigma?onclick:sigma);
+  };
   return (
-    <div>   
-       <h2>Profile page</h2>
-       <button onClick={logout}>Logout</button>
-    </div>
-  )
+    <main>
+      <div className="block_count">
+        <img src="../../public/img/coin_click.png" alt="error" />
+        <h2>{count}</h2>
+      </div>
+      <div className="block_click">
+        <img src={image} alt="error" onClick={clickCoin}/>
+      </div>
+    </main>
+  );
 }
